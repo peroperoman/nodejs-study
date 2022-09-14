0446d49622d1b8a60b8ca361d6602e2e51a6a46b
Masaya Nakano <isono.sendai@gmail.com>
2022-09-14 21:52:44 +0900

    docker環境へ移行

---
+++ app/db/connect.js
@@ -0,0 +1,11 @@
+// getting-started.js
+const mongoose = require("mongoose");
+
+const connectDB = (url) => {
+    return mongoose
+        .connect(url)
+        .then(() => console.log("データベースと接続中・・・"))
+        .catch((err) => console.log(err));
+};
+
+module.exports = connectDB;
